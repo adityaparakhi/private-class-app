95dbba6965e34f32b413ae9783b0595836f7d74b
Aditya Rajendra Parakhi <80239927+adityaparakhi@users.noreply.github.com>
2025-08-28 19:13:56 +0530

    Delete frontend directory

--- frontend/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-center">
-      <h1>Welcome to Private Classes</h1>
-      <p>Your learning journey starts here.</p>
-    </div>
-  );
-}
