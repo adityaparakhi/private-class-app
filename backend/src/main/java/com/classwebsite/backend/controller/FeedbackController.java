3df9429f7e443064b17b2752e4271e09ea075eaf
Aditya Parkhi <adityaparakhi.1998@gmail.com>
2025-08-28 19:23:21 +0530

    Initial commit: add frontend and backend folders

---
+++ backend/src/main/java/com/classwebsite/backend/controller/FeedbackController.java
@@ -0,0 +1,27 @@
+package com.classwebsite.backend.controller;
+
+import com.classwebsite.backend.model.Feedback;
+import com.classwebsite.backend.repository.FeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/feedback")
+@CrossOrigin(origins = "http://localhost:3000") // React port
+public class FeedbackController {
+
+    @Autowired
+    private FeedbackRepository feedbackRepository;
+
+    @PostMapping
+    public Feedback saveFeedback(@RequestBody Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    @GetMapping
+    public List<Feedback> getAllFeedback() {
+        return feedbackRepository.findAll();
+    }
+}
